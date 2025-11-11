23ee8df9d0a011ce50b88faa2720413e47461ce8
DouglasBezague15 <SEU_EMAIL@exemplo.com>
2025-11-10 22:03:54 -0300

    Remove pasta src

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Carro carro = new Carro("Toyota", "Corolla", 2020, 4);
-        Moto moto = new Moto("Honda", "CB 500", 2022, 500);
-
-        System.out.println(carro.informacoesVeiculo());
-        System.out.println(moto.informacoesVeiculo());
-    }
-}
